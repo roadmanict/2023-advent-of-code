89bdf8dbe90207ebd1a48a1528abd5c8aed7e92b
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2023-11-30 12:08:32 +0100

    Add read to file example

--- tests/day_1.rs
+++ tests/day_1.rs
@@ -1,7 +1,11 @@
+use std::fs;
+
 use adventofcode2023::add;
 
 #[test]
 fn test_day_1_part_1() {
+    let _file = fs::read_to_string("resources/day_1.txt").expect("File should be available");
+
     assert_eq!(add(1,3), 4);
 }
 
