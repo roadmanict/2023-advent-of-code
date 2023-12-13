365ad33a93474490106767da3c5ee6c7a8abc324
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2023-12-13 14:37:11 +0100

    Solve day 7 part 1

--- src/camel_hand.rs
+++ src/camel_hand.rs
@@ -1,8 +1,46 @@
-use std::{num::ParseIntError, str::FromStr};
+use std::{
+    collections::HashMap,
+    num::{ParseIntError, TryFromIntError},
+    str::FromStr,
+};
 
 use thiserror::Error;
 
-pub struct CamelHand {}
+#[derive(Debug, PartialEq, Ord, Eq)]
+pub struct CamelHand {
+    cards: Vec<CamelCard>,
+    first_card: CamelCard,
+    pub bid: usize,
+    strength: CamelHandType,
+}
+
+impl CamelHand {
+    pub fn new(
+        cards: Vec<CamelCard>,
+        first_card: CamelCard,
+        bid: usize,
+        strength: CamelHandType,
+    ) -> Self {
+        Self {
+            cards,
+            first_card,
+            bid,
+            strength,
+        }
+    }
+}
+
+impl PartialOrd for CamelHand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let result = self.strength.partial_cmp(&other.strength)?;
+
+        if result == std::cmp::Ordering::Equal {
+            return other.cards.partial_cmp(&self.cards)
+        }
+
+        Some(result)
+    }
+}
 
 #[derive(Debug, Error)]
 pub enum CamelHandParseError {
@@ -10,8 +48,11 @@ pub enum CamelHandParseError {
     InvalidInputError,
     #[error("Can't parse number error")]
     ParseIntError(#[from] ParseIntError),
+    #[error("Can't parse number error")]
+    TryFromIntError(#[from] TryFromIntError),
 }
 
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
 pub enum CamelHandType {
     FiveOfAKind,
     FourOfAKind,
@@ -22,17 +63,36 @@ pub enum CamelHandType {
     HighCard,
 }
 
-pub struct CamelCard(char);
+#[derive(Debug, PartialEq, Eq, Ord, Clone)]
+pub struct CamelCard(char, usize);
 
-impl CamelCard {
-    pub fn value(&self) -> usize {
-        todo!()
+impl PartialOrd for CamelCard {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.1.partial_cmp(&other.1)
     }
 }
 
-impl From<char> for CamelCard {
-    fn from(value: char) -> Self {
-        CamelCard(value)
+impl TryFrom<char> for CamelCard {
+    type Error = CamelHandParseError;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        let usize_value: usize = match value {
+            x if x.is_digit(10) => {
+                let raw_value = x
+                    .to_digit(10)
+                    .ok_or(CamelHandParseError::InvalidInputError)?;
+
+                Ok(raw_value.try_into()?)
+            }
+            'T' => Ok(10),
+            'J' => Ok(11),
+            'Q' => Ok(12),
+            'K' => Ok(13),
+            'A' => Ok(14),
+            _ => Err(CamelHandParseError::InvalidInputError),
+        }?;
+
+        Ok(CamelCard(value, usize_value))
     }
 }
 
@@ -44,8 +104,45 @@ impl FromStr for CamelHand {
             .split_once(' ')
             .ok_or(CamelHandParseError::InvalidInputError)?;
 
-        let cards: Vec<CamelCard> = hand.chars().map(|c| c.into()).collect();
-        todo!()
+        let cards: Vec<CamelCard> = hand
+            .chars()
+            .map(|c| CamelCard::try_from(c))
+            .collect::<Result<Vec<_>, _>>()?;
+        let first_card = cards
+            .get(0)
+            .ok_or(CamelHandParseError::InvalidInputError)?
+            .to_owned();
+
+        let mut card_count = HashMap::new();
+
+        for card in cards.iter() {
+            if card_count.contains_key(&card.0) {
+                continue;
+            }
+            let count = cards.iter().filter(|c| c.0 == card.0).count();
+
+            card_count.insert(card.0, (count, card));
+        }
+
+        let mut counted_cards: Vec<&(usize, &CamelCard)> = card_count.values().collect::<Vec<_>>();
+        counted_cards.sort_by(|a, b| b.0.cmp(&a.0));
+
+        println!("{:?}", counted_cards);
+
+        let strength = match counted_cards.as_slice() {
+            [(5, _), ..] => Ok(CamelHandType::FiveOfAKind),
+            [(4, _), ..] => Ok(CamelHandType::FourOfAKind),
+            [(3, _), (2, _), ..] => Ok(CamelHandType::FullHouse),
+            [(3, _), ..] => Ok(CamelHandType::ThreeOfAKind),
+            [(2, _), (2, _), ..] => Ok(CamelHandType::TwoPair),
+            [(2, _), ..] => Ok(CamelHandType::OnePair),
+            [(1, _), ..] => Ok(CamelHandType::HighCard),
+            _ => Err(CamelHandParseError::InvalidInputError),
+        };
+
+        println!("{:?}", strength);
+
+        Ok(CamelHand::new(cards, first_card, bid.parse()?, strength?))
     }
 }
 
@@ -56,5 +153,32 @@ mod tests {
     #[test]
     fn test_camel_card_one_pair() {
         let hand: CamelHand = "32T3K 765".parse().expect("Should parse");
+
+        assert_eq!(hand.strength, CamelHandType::OnePair);
+    }
+
+    #[test]
+    fn test_camel_card_three_of_a_kind() {
+        let hand: CamelHand = "T55J5 684".parse().expect("Should parse");
+
+        assert_eq!(hand.strength, CamelHandType::ThreeOfAKind);
+    }
+
+    #[test]
+    fn test_camel_card_sort() {
+        let mut hands: Vec<CamelHand> = vec![
+            "32T3K 765".parse().expect("Should parse"),
+            "T55J5 684".parse().expect("Should parse"),
+            "A55J5 684".parse().expect("Should parse"),
+            "KK677 28".parse().expect("Should parse"),
+        ];
+
+        hands.sort();
+
+        assert_eq!(hands[0].strength, CamelHandType::ThreeOfAKind);
+        assert_eq!(hands[0].cards[0].0, 'A');
+        assert_eq!(hands[1].strength, CamelHandType::ThreeOfAKind);
+        assert_eq!(hands[2].strength, CamelHandType::TwoPair);
+        assert_eq!(hands[3].strength, CamelHandType::OnePair);
     }
 }
--- tests/day_7.rs
+++ tests/day_7.rs
@@ -6,8 +6,16 @@ use adventofcode2023::camel_hand::CamelHand;
 fn test_day_7_part_1() {
     let file = fs::read_to_string("resources/day_7.txt").expect("File should be available");
 
-    for line in file.lines() {
+    let mut hands: Vec<CamelHand> = file
+        .lines()
+        .map(str::parse)
+        .collect::<Result<Vec<_>, _>>()
+        .expect("Should parse");
 
-        let _camel_hand: CamelHand = line.parse().expect("Should parse");
-    }
+    hands.sort();
+    hands.reverse();
+
+    let result: usize = hands.iter().enumerate().map(|(i, hand)| (i+1) * hand.bid).sum();
+
+    assert_eq!(result, 251106089);
 }
