ed81148d68af81fa6febf50e82c15e588acbd4b4
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2023-12-15 16:04:00 +0100

    Solve day 7 part 2

--- src/camel_hand.rs
+++ src/camel_hand.rs
@@ -1,39 +1,9 @@
 use std::{
     collections::HashMap,
     num::{ParseIntError, TryFromIntError},
-    str::FromStr,
 };
 
 use thiserror::Error;
-
-#[derive(Debug, PartialEq, Ord, Eq)]
-pub struct CamelHand {
-    cards: Vec<CamelCard>,
-    pub bid: usize,
-    strength: CamelHandType,
-}
-
-impl CamelHand {
-    pub fn new(cards: Vec<CamelCard>, bid: usize, strength: CamelHandType) -> Self {
-        Self {
-            cards,
-            bid,
-            strength,
-        }
-    }
-}
-
-impl PartialOrd for CamelHand {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        let result = self.strength.partial_cmp(&other.strength)?;
-
-        if result == std::cmp::Ordering::Equal {
-            return other.cards.partial_cmp(&self.cards);
-        }
-
-        Some(result)
-    }
-}
 
 #[derive(Debug, Error)]
 pub enum CamelHandParseError {
@@ -45,78 +15,121 @@ pub enum CamelHandParseError {
     TryFromIntError(#[from] TryFromIntError),
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
-pub enum CamelHandType {
-    FiveOfAKind,
-    FourOfAKind,
-    FullHouse,
-    ThreeOfAKind,
-    TwoPair,
-    OnePair,
-    HighCard,
+#[derive(Debug)]
+pub enum GameType {
+    WithJoker,
+    WithoutJoker,
 }
 
-#[derive(Debug, PartialEq, Eq, Ord, Clone)]
-pub struct CamelCard(char, usize);
-
-impl PartialOrd for CamelCard {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.1.partial_cmp(&other.1)
-    }
+#[derive(Debug, PartialEq, Ord, PartialOrd, Eq, Hash)]
+pub enum CamelCard {
+    Joker,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
 }
 
-impl TryFrom<char> for CamelCard {
+impl TryFrom<(char, &GameType)> for CamelCard {
     type Error = CamelHandParseError;
 
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        let usize_value: usize = match value {
-            x if x.is_ascii_digit() => {
-                let raw_value = x
-                    .to_digit(10)
-                    .ok_or(CamelHandParseError::InvalidInputError)?;
-
-                Ok(raw_value.try_into()?)
-            }
-            'T' => Ok(10),
-            'J' => Ok(11),
-            'Q' => Ok(12),
-            'K' => Ok(13),
-            'A' => Ok(14),
+    fn try_from(value: (char, &GameType)) -> Result<Self, Self::Error> {
+        match value {
+            ('A', _) => Ok(CamelCard::Ace),
+            ('K', _) => Ok(CamelCard::King),
+            ('Q', _) => Ok(CamelCard::Queen),
+            ('J', GameType::WithoutJoker) => Ok(CamelCard::Jack),
+            ('J', GameType::WithJoker) => Ok(CamelCard::Joker),
+            ('T', _) => Ok(CamelCard::Ten),
+            ('9', _) => Ok(CamelCard::Nine),
+            ('8', _) => Ok(CamelCard::Eight),
+            ('7', _) => Ok(CamelCard::Seven),
+            ('6', _) => Ok(CamelCard::Six),
+            ('5', _) => Ok(CamelCard::Five),
+            ('4', _) => Ok(CamelCard::Four),
+            ('3', _) => Ok(CamelCard::Three),
+            ('2', _) => Ok(CamelCard::Two),
             _ => Err(CamelHandParseError::InvalidInputError),
-        }?;
-
-        Ok(CamelCard(value, usize_value))
+        }
     }
 }
 
-impl FromStr for CamelHand {
-    type Err = CamelHandParseError;
+#[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
+pub enum CamelHandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let (hand, bid) = s
+#[derive(Debug, PartialEq, Ord, Eq)]
+pub struct CamelHand {
+    cards: Vec<CamelCard>,
+    pub bid: usize,
+    pub strength: CamelHandType,
+}
+
+impl TryFrom<(&str, GameType)> for CamelHand {
+    type Error = CamelHandParseError;
+
+    fn try_from((value, game_type): (&str, GameType)) -> Result<Self, Self::Error> {
+        let (hand, bid) = value
             .split_once(' ')
             .ok_or(CamelHandParseError::InvalidInputError)?;
 
         let cards: Vec<CamelCard> = hand
             .chars()
+            .map(|c| (c, &game_type))
             .map(CamelCard::try_from)
             .collect::<Result<Vec<_>, _>>()?;
 
-        let mut card_count = HashMap::new();
+        let mut card_count: HashMap<&CamelCard, (usize, &CamelCard)> = HashMap::new();
+        let joker_count = cards.iter().filter(|c| c == &&CamelCard::Joker).count();
 
         for card in cards.iter() {
-            if card_count.contains_key(&card.0) {
+            if card_count.contains_key(card.to_owned()) {
                 continue;
             }
-            let count = cards.iter().filter(|c| c.0 == card.0).count();
+            let count = cards.iter().filter(|c| c == &card).count();
 
-            card_count.insert(card.0, (count, card));
+            card_count.insert(card, (count, &card));
         }
 
-        let mut counted_cards: Vec<&(usize, &CamelCard)> = card_count.values().collect::<Vec<_>>();
+        let mut counted_cards: Vec<(usize, &CamelCard)> =
+            card_count.into_iter().map(|(_, v)| v).collect::<Vec<_>>();
         counted_cards.sort_by(|a, b| b.0.cmp(&a.0));
 
-        println!("{:?}", counted_cards);
+        if joker_count > 0 && joker_count < 5 {
+            let mut joker_index: Option<usize> = None;
+            for (i, counted_card) in counted_cards.iter_mut().enumerate() {
+                if counted_card.1 == &CamelCard::Joker {
+                    joker_index = Some(i);
+
+                    continue;
+                }
+
+                counted_card.0 += joker_count;
+
+                break;
+            }
+
+            if let Some(i) = joker_index {
+                counted_cards.remove(i);
+
+            }
+        }
 
         let strength = match counted_cards.as_slice() {
             [(5, _), ..] => Ok(CamelHandType::FiveOfAKind),
@@ -127,11 +140,25 @@ impl FromStr for CamelHand {
             [(2, _), ..] => Ok(CamelHandType::OnePair),
             [(1, _), ..] => Ok(CamelHandType::HighCard),
             _ => Err(CamelHandParseError::InvalidInputError),
-        };
+        }?;
 
-        println!("{:?}", strength);
+        Ok(CamelHand {
+            cards,
+            bid: bid.parse()?,
+            strength,
+        })
+    }
+}
 
-        Ok(CamelHand::new(cards, bid.parse()?, strength?))
+impl PartialOrd for CamelHand {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let result = self.strength.partial_cmp(&other.strength)?;
+
+        if result == std::cmp::Ordering::Equal {
+            return self.cards.partial_cmp(&other.cards);
+        }
+
+        Some(result)
     }
 }
 
@@ -140,15 +167,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_camel_card() {
+        let joker: CamelCard = ('J', &GameType::WithJoker).try_into().expect("Should work");
+        assert_eq!(joker, CamelCard::Joker);
+        let jack: CamelCard = ('J', &GameType::WithoutJoker)
+            .try_into()
+            .expect("Should work");
+        assert_eq!(jack, CamelCard::Jack);
+        let two: CamelCard = ('2', &GameType::WithoutJoker)
+            .try_into()
+            .expect("Should work");
+        assert_eq!(two, CamelCard::Two);
+
+        assert_eq!(joker.partial_cmp(&jack), Some(core::cmp::Ordering::Less));
+        assert_eq!(joker.partial_cmp(&two), Some(core::cmp::Ordering::Less));
+    }
+
+    #[test]
     fn test_camel_card_one_pair() {
-        let hand: CamelHand = "32T3K 765".parse().expect("Should parse");
+        let hand: CamelHand = ("32T3K 765", GameType::WithoutJoker)
+            .try_into()
+            .expect("Should parse");
 
         assert_eq!(hand.strength, CamelHandType::OnePair);
     }
 
     #[test]
     fn test_camel_card_three_of_a_kind() {
-        let hand: CamelHand = "T55J5 684".parse().expect("Should parse");
+        let hand: CamelHand = ("T55J5 684", GameType::WithoutJoker)
+            .try_into()
+            .expect("Should parse");
 
         assert_eq!(hand.strength, CamelHandType::ThreeOfAKind);
     }
@@ -156,18 +204,35 @@ mod tests {
     #[test]
     fn test_camel_card_sort() {
         let mut hands: Vec<CamelHand> = vec![
-            "32T3K 765".parse().expect("Should parse"),
-            "T55J5 684".parse().expect("Should parse"),
-            "A55J5 684".parse().expect("Should parse"),
-            "KK677 28".parse().expect("Should parse"),
+            ("32T3K 765", GameType::WithoutJoker)
+                .try_into()
+                .expect("Should parse"),
+            ("T55J5 684", GameType::WithoutJoker)
+                .try_into()
+                .expect("Should parse"),
+            ("A55J5 684", GameType::WithoutJoker)
+                .try_into()
+                .expect("Should parse"),
+            ("KK677 28", GameType::WithoutJoker)
+                .try_into()
+                .expect("Should parse"),
         ];
 
         hands.sort();
 
-        assert_eq!(hands[0].strength, CamelHandType::ThreeOfAKind);
-        assert_eq!(hands[0].cards[0].0, 'A');
-        assert_eq!(hands[1].strength, CamelHandType::ThreeOfAKind);
-        assert_eq!(hands[2].strength, CamelHandType::TwoPair);
-        assert_eq!(hands[3].strength, CamelHandType::OnePair);
+        assert_eq!(hands[0].strength, CamelHandType::OnePair);
+        assert_eq!(hands[1].strength, CamelHandType::TwoPair);
+        assert_eq!(hands[2].strength, CamelHandType::ThreeOfAKind);
+        assert_eq!(hands[3].cards[0], CamelCard::Ace);
+        assert_eq!(hands[3].strength, CamelHandType::ThreeOfAKind);
+    }
+
+    #[test]
+    fn test_camel_card_jokerr() {
+        let hand: CamelHand = ("JKKJA 765", GameType::WithJoker)
+            .try_into()
+            .expect("Should parse");
+
+        assert_eq!(hand.strength, CamelHandType::FourOfAKind);
     }
 }
--- tests/day_7.rs
+++ tests/day_7.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use adventofcode2023::camel_hand::CamelHand;
+use adventofcode2023::camel_hand::{CamelHand, GameType};
 
 #[test]
 fn test_day_7_part_1() {
@@ -8,14 +8,38 @@ fn test_day_7_part_1() {
 
     let mut hands: Vec<CamelHand> = file
         .lines()
-        .map(str::parse)
+        .map(|s| CamelHand::try_from((s, GameType::WithoutJoker)))
         .collect::<Result<Vec<_>, _>>()
         .expect("Should parse");
 
     hands.sort();
-    hands.reverse();
 
-    let result: usize = hands.iter().enumerate().map(|(i, hand)| (i+1) * hand.bid).sum();
+    let result: usize = hands
+        .iter()
+        .enumerate()
+        .map(|(i, hand)| (i + 1) * hand.bid)
+        .sum();
 
     assert_eq!(result, 251106089);
+}
+
+#[test]
+fn test_day_7_part_2() {
+    let file = fs::read_to_string("resources/day_7.txt").expect("File should be available");
+
+    let mut hands: Vec<CamelHand> = file
+        .lines()
+        .map(|s| CamelHand::try_from((s, GameType::WithJoker)))
+        .collect::<Result<Vec<_>, _>>()
+        .expect("Should parse");
+
+    hands.sort();
+
+    let result: usize = hands
+        .iter()
+        .enumerate()
+        .map(|(i, hand)| (i + 1) * hand.bid)
+        .sum();
+
+    assert_eq!(result, 249620106);
 }
